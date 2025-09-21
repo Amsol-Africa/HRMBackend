fd31a2c72c37e657da67c1645b1cdea856411dd1
Tracy-miranja <barazatracy44@gmail.com>
2025-09-21 22:02:14 +0300

    protection

--- app/Libraries/PdfWithProtection.php
+++ app/Libraries/PdfWithProtection.php
@@ -1,10 +1,25 @@
 <?php
 namespace App\Libraries;
 
-class PdfWithProtection extends \App\Libraries\FPDF_Protection
+use setasign\Fpdi\Fpdi;
+
+require_once __DIR__ . '/Fpdf/fpdf_protection.php';
+
+class PdfWithProtection extends Fpdi
 {
+    use \FPDF_Protection_Trait; // if your fpdf_protection is a trait, otherwise extend the class
+
     public function __construct($orientation = 'P', $unit = 'mm', $size = 'A4')
     {
         parent::__construct($orientation, $unit, $size);
+    }
+
+    /**
+     * Wrapper for protection method
+     */
+    public function SetProtection($permissions = [], $user_pass = '', $owner_pass = null)
+    {
+        // Call method from FPDF_Protection
+        return parent::SetProtection($permissions, $user_pass, $owner_pass);
     }
 }
